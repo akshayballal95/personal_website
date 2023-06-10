89026e4708ccf51e9cb513c7e7de63a7d7a31be5
= <=>
2023-06-10 23:48:07 +0530

    Google Title

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -20,6 +20,7 @@
 		name="description"
 		content="Welcome to my personal website! I am a self-taught AI developer driven by a passion for pushing the boundaries of technology. Applying First Principles thinking, I strive to solve complex challenges and create innovative solutions. As a Technology Enthusiast, I constantly explore the latest advancements in the field. I am deeply committed to leveraging AI for social good and advocate for green technology. Join me on this journey as I utilize my self-taught expertise to build products and technologies that address industrial problems. Let's collaborate and shape a better future together."
 	/>
+	<meta name="title" content="Akshay Ballal: Machine Learning Enthusiast">
 </svelte:head>
 
 <div class="h-full flex items-center p-3 xl:p-12">
