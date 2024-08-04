4a1776b97d814b6ab1ffacc551d10a8029b62ab0
Akshay Ballal <arballal95@gmail.com>
2024-08-04 23:44:58 +0530

    chore: Update image source attributes and add og:image meta tags

--- src/routes/(app)/blogs/[blog_id]/+page.server.ts
+++ src/routes/(app)/blogs/[blog_id]/+page.server.ts
@@ -8,6 +8,7 @@ export async function load({ params }) {
         id: params.blog_id,
         title: blog.metadata.title,
         html: blog.default.render().html,
+        image_url: blog.metadata.image,
         description: blog.metadata.description,
         date:new Date( blog.metadata.date),
     }
--- src/routes/(app)/blogs/[blog_id]/+page.svelte
+++ src/routes/(app)/blogs/[blog_id]/+page.svelte
@@ -3,7 +3,7 @@
 	import { onMount } from 'svelte';
 
 	/** @type {import('./$types').PageData} */
-	export let data: { id: string; date: Date; title: string; html: string; description: string };
+	export let data: { id: string; date: Date; title: string; html: string; description: string; image_url: string };
 
 	function formattedDate(date: Date) {
 		return date
@@ -19,17 +19,36 @@
 		$tabSet = 4;
 		document.body.querySelector('#blog')?.scrollTo(0, 0);
 	});
-
 </script>
 
 <svelte:head>
 	<title>{data.title}</title>
 	<meta name="description" content={data.description} />
 	<meta name="title" content={data.title} />
-</svelte:head>
+	<meta
+		property="og:image"
+		content={data.image_url}
+	/>
+	<meta
+		property="og:description"
+		content={data.description}/>
+	<meta property="og:title" content={data.title}/>
+	<meta property="og:description" content={data.description} />
+	
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:creator" content="@akshayballal95" />
+	<meta property="twitter:image"
+		content={data.image_url} />
+	<meta property="twitter:title" content={data.title} />
+	<meta property="twitter:description"
+		content={data.description}/>
+
+		<meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+		</svelte:head
+>
 
 <svelte:window bind:scrollY={y} />
-
 
 <div class="flex justify-center lg:p-10 p-5 mx-auto overflow-hidden">
 	<div id="blog" class="flex flex-col gap-8 md:w-3/4 max-w-4xl rounded-md card md:p-8 p-5 relative">
